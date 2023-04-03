8f94ad65792a764001aed52312b352a3d90644c6
achrafaitibba <97747197+achrafaitibba@users.noreply.github.com>
2023-04-03 15:46:30 +0000

    Updating the dataSource to get the apiKey using Value annotation and to keep the keys safe inside the application.properties file

--- com/onexshield/datacollector/dataSource/PlacesApiDataSource.java
+++ com/onexshield/datacollector/dataSource/PlacesApiDataSource.java
@@ -1,6 +1,7 @@
 package com.onexshield.datacollector.dataSource;
 
 import com.google.maps.GeoApiContext;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.FileInputStream;
@@ -10,24 +11,24 @@ import java.util.Properties;
 @Component
 public class PlacesApiDataSource {
 
+    @Value("${App.apiKey}")
+    private String apiKey;
 
-    public String getApiKey() throws Exception{
-        Properties properties=new Properties();
-        try (InputStream is = new FileInputStream("src/main/resources/application.properties")) {
-            properties.load(is);
-        }
-        return properties.getProperty("apiKey");
-    }
+//    public String getApiKey() throws Exception{
+//        Properties properties=new Properties();
+//        try (InputStream is = new FileInputStream("src/main/resources/application.properties")) {
+//            properties.load(is);
+//        }
+//        return properties.getProperty("apiKey");
+//    }
     public GeoApiContext getApiAccess()throws Exception{
         // Set up the GeoApiContext with your API key
         return new GeoApiContext.Builder()
-                .apiKey(getApiKey())
+                .apiKey(apiKey)
                 .build();
     }
 
-
-
-
-
-
+    public String getApiKey() {
+        return apiKey;
+    }
 }
