932f6f0b17aae1e47e73d0a15cb9c6724fd9062f
achrafaitibba <97747197+achrafaitibba@users.noreply.github.com>
2023-04-04 23:57:18 +0000

    Updating the dataSource , getting the apiKey from an external database to facilitate updating the apiKEY remotly without hardcoding it

--- com/onexshield/datacollector/dataSource/PlacesApiDataSource.java
+++ com/onexshield/datacollector/dataSource/PlacesApiDataSource.java
@@ -1,34 +1,25 @@
 package com.onexshield.datacollector.dataSource;
 
 import com.google.maps.GeoApiContext;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.Properties;
 
 @Component
 public class PlacesApiDataSource {
 
-    @Value("${App.apiKey}")
-    private String apiKey;
+    private apiKeyDao apiKey;
 
-//    public String getApiKey() throws Exception{
-//        Properties properties=new Properties();
-//        try (InputStream is = new FileInputStream("src/main/resources/application.properties")) {
-//            properties.load(is);
-//        }
-//        return properties.getProperty("apiKey");
-//    }
+    @Autowired
+    public PlacesApiDataSource(apiKeyDao apiKey){
+        this.apiKey = apiKey;
+    }
     public GeoApiContext getApiAccess()throws Exception{
         // Set up the GeoApiContext with your API key
         return new GeoApiContext.Builder()
-                .apiKey(apiKey)
+                .apiKey(apiKey.getApiKey())
                 .build();
     }
 
-    public String getApiKey() {
-        return apiKey;
-    }
+
 }
---
+++ com/onexshield/datacollector/dataSource/apiKeyDao.java
@@ -0,0 +1,32 @@
+package com.onexshield.datacollector.dataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+@Component
+public class apiKeyDao {
+
+    private Connection connection;
+
+    @Autowired
+    public apiKeyDao(Connection connection){
+        this.connection = connection;
+    }
+
+
+    public String getApiKey() throws Exception{
+        String sql = "select keyString from keysValues where id = ?";
+        PreparedStatement statement = connection.getConnected().prepareStatement(sql);
+        statement.setInt(1,1);
+        ResultSet resultSet = statement.executeQuery();
+        String key = "";
+        while(resultSet.next()){
+            key = resultSet.getString(1);
+        }
+        return  key;
+    }
+}
