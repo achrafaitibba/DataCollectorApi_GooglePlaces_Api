0458f7649cd1f8a3dca17b84db59bcfb4b116706
Achraf Ait Ibba <97747197+achrafaitibba@users.noreply.github.com>
2023-05-31 13:30:17 +0000

    adding the whole project for more coming updates

---
+++ src/test/java/com/onexshield/datacollector/testingDataFetch.java
@@ -0,0 +1,21 @@
+package com.onexshield.datacollector;
+
+import com.onexshield.datacollector.client.responseRepository;
+import com.onexshield.datacollector.dataSource.PlacesApiDataSource;
+import com.onexshield.datacollector.model.request;
+import com.onexshield.datacollector.model.response;
+
+import java.util.ArrayList;
+
+public class testingDataFetch {
+    public static void main(String[] args) throws Exception{
+//        responseRepository responseRepository = new responseRepository(new PlacesApiDataSource());
+//        ArrayList<response> data = responseRepository.getData(new request("restaurant","rabat","morocco",0));
+//
+//        for (response re : data){
+//            System.out.println(re.getAddress()  + re.getCity() + re.getCountry() + re.getOpeningHours());
+//        }
+//
+
+    }
+}
